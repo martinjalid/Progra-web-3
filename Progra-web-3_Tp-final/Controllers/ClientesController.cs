70741fe22f8341d5281d5dd36c59931cc2508cf2
Martin Jalid <martin.jalid@mercadolibre.com>
2021-05-14 17:35:16 -0300

    vista clientes

---
+++ Progra-web-3_Tp-final/Controllers/ClientesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Progra_web_3_Tp_final.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private readonly ILogger<ClientesController> _logger;
+
+        public ClientesController(ILogger<ClientesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
