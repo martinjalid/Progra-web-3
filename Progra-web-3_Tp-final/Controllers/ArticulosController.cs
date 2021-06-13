1de4d9a5d74b3412cb58fc684afa484246aa54d1
ldinubila <lucas.dinubila@gmail.com>
2021-06-12 22:35:04 -0300

    Los articulos se cargar desde BD

--- Progra-web-3_Tp-final/Controllers/ArticulosController.cs
+++ Progra-web-3_Tp-final/Controllers/ArticulosController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Progra_web_3_Tp_final.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,16 @@ namespace Progra_web_3_Tp_final.Controllers
 {
     public class ArticulosController : Controller
     {
+        _20211CTPContext context;
+
+        public ArticulosController()
+        {
+            context = new _20211CTPContext();
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(context.Articulos.ToList());
         }
 
         public IActionResult NuevoArticulo()
