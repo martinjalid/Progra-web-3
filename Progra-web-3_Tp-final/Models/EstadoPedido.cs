4804d56301b88bfa5219c476ac6224675ebec188
ldinubila <lucas.dinubila@gmail.com>
2021-06-12 21:21:31 -0300

    Se creo la conexion con la BD

---
+++ Progra-web-3_Tp-final/Models/EstadoPedido.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Progra_web_3_Tp_final.Models
+{
+    public partial class EstadoPedido
+    {
+        public EstadoPedido()
+        {
+            Pedidos = new HashSet<Pedido>();
+        }
+
+        public int IdEstadoPedido { get; set; }
+        public string Descripcion { get; set; }
+
+        public virtual ICollection<Pedido> Pedidos { get; set; }
+    }
+}
