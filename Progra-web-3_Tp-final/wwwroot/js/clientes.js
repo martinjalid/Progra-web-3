eb8644f2b795954742b5c22cbf286e5df5401f86
Martin Jalid <martin.jalid@mercadolibre.com>
2021-07-05 17:53:13 -0300

    fix filters

--- Progra-web-3_Tp-final/wwwroot/js/clientes.js
+++ Progra-web-3_Tp-final/wwwroot/js/clientes.js
@@ -1,5 +1,23 @@
 ﻿$(document).ready(() => {
-    $('#example').DataTable();
+    var table = $('#tabla_clientes').DataTable({
+        "searching": false,
+        "info": false,
+        "order": [[1, "asc"]],
+        initComplete: function () {
+            // Apply the search
+            this.api().columns().every(function () {
+                var that = this;
+
+                $('input', this.footer()).on('keyup change clear', function () {
+                    if (that.search() !== this.value) {
+                        that
+                            .search(this.value)
+                            .draw();
+                    }
+                });
+            });
+        }
+    });
 
     $("#guardar").click(() => {
         const data = collectData();
@@ -131,3 +149,73 @@
  
     }
 });
+
+function filtro_nombre() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("buscar_nombre");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tabla_clientes");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+};
+
+function filtro_numero() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("buscar_numero");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tabla_clientes");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+        }
+    }
+};
+
+function filtro_eliminados() {
+    // Declare variables
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("excluir_eliminados");
+    filter = input.checked;
+    table = document.getElementById("tabla_clientes");
+    tr = table.getElementsByTagName("tr");
+
+    // Loop through all table rows, and hide those who don't match the search query
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByClassName("fecha-borrado")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (filter) {
+                if (txtValue === '') {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            } else {
+                tr[i].style.display = "";
+            }
+        }
+    }
+};
